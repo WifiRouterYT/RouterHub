2f750aeb5a26ee26a9e1c653c90938b12b108731
WifiRouter <45213292+WifiRouterYT@users.noreply.github.com>
2022-06-06 19:30:23 -0700

    Create gamenotfound.lua

---
+++ gamenotfound.lua
@@ -0,0 +1,4 @@
+-- lib
+local Lib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/BoredStuff2/notify-lib/main/lib'),true))()
+
+Lib.prompt('Error!', 'Game is unsupported. Supported games: Natural Disaster Survival, Destruction Simulator', 3)
