da791336186b70eaf6405c27ae13e94556f63e7b
WifiRouter <45213292+WifiRouterYT@users.noreply.github.com>
2022-06-06 19:31:08 -0700

    Create DS.lua

---
+++ DS.lua
@@ -0,0 +1,5 @@
+
+-- lib
+local Lib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/BoredStuff2/notify-lib/main/lib'),true))()
+
+Lib.prompt('Error!', 'This script is not finished yet. Expected release: 2-5 days.', 5)
