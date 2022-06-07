7223cad14391e15f0bf5cd55a41e069dd10e9322
WifiRouter <45213292+WifiRouterYT@users.noreply.github.com>
2022-06-06 19:25:06 -0700

    loader 1!!

---
+++ loader.lua
@@ -0,0 +1,5 @@
+if(game.PlaceId == 189707) then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/WifiRouterYT/RouterHub/main/NDS.lua"))()
+	print("Detected game - Natural Disaster Survival")
+	print("Loading script.")
+end
